5072bddd75dee07da6881db95a33fa9315cf4aea
kkrasilov <kkrasilov@gmail.com>
2020-11-27 21:32:35 +0500

    refactoring code

--- lib/films_reader.rb
+++ lib/films_reader.rb
@@ -5,17 +5,19 @@ require 'nokogiri'
 module FilmsReader
   module_function
 
-  def films_from_url(url)
-    html = URI.open(url)
+  WIKI_URL = 'https://ru.wikipedia.org/wiki/250_лучших_фильмов_по_версии_IMDb'.freeze
+
+  def from_wiki_imdb250
+    html = URI.open(URI.escape(WIKI_URL))
 
     document = Nokogiri::HTML(html)
 
     document.css("tbody tr").drop(1).map do |item|
-      Film.new({
-                 title: item.css('td')[1].css('a').text,
-                 director: item.css('td')[3].css('a').text,
-                 year: item.css('td')[2].css('a').text
-               })
+      Film.new(
+        title: item.css('td')[1].css('a').text,
+        director: item.css('td')[3].css('a').text,
+        year: item.css('td')[2].css('a').text
+      )
     end
   end
 end
--- main.rb
+++ main.rb
@@ -4,11 +4,7 @@
 require_relative 'lib/films_reader'
 require_relative 'lib/films_collection'
 
-# IMDb Charts
-# Top Rated Movies
-url = 'https://ru.wikipedia.org/wiki/250_%D0%BB%D1%83%D1%87%D1%88%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B2_%D0%BF%D0%BE_%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8_IMDb'
-
-films = FilmsReader.films_from_url(url)
+films = FilmsReader.from_wiki_imdb250
 
 films_collection = FilmsCollection.new(films)
 
