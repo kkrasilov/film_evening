48134e2e2af3f2949e5f340d26db1f844656b8a4
kkrasilov <kkrasilov@gmail.com>
2020-12-19 14:38:13 +0500

    fix test films reader by new method

--- spec/films_reader_spec.rb
+++ spec/films_reader_spec.rb
@@ -4,14 +4,13 @@ require 'film'
 describe FilmsReader do
   describe '.films_from_url' do
     it 'read all movies from the site and return movies' do
-      things = described_class.films_from_url("#{__dir__}/fixtures/index.html")
+      things = described_class.from_wiki_imdb250
 
       expect(things).to be_an Array
       expect(things).to all be_a Film
-      expect(things.size).to eq 3
+      expect(things.size).to eq 250
       expect(things[0].year).to eq 1994
-      expect(things.map(&:director)).to contain_exactly 'Frank Darabont',
-                                                        'Francis Ford Coppola', 'Christopher Nolan'
+      expect(things.map(&:director).first(2)).to contain_exactly 'Фрэнк Дарабонт', 'Фрэнсис Форд Коппола'
     end
   end
 end
